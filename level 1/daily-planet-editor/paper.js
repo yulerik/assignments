fca3b517ad9182a2e6ea99f8ae192ab3b3348e52
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2021-09-29 13:39:09 -0700

    fixed program errors, but not program functionality. variable names capitalized, white spaced added, indentation fixed. typos fixed.

--- level 1/daily-planet-editor/paper.js
+++ level 1/daily-planet-editor/paper.js
@@ -8,47 +8,51 @@ just fix the existing ones.
 
 
 // Written by Kent, Clark
+// change var capital letter, white space in/around array
+var enemies = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
 
-var Enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
-
-funtion Who_Wins(is-there-kryptonite,enemy_name)
-{
-if (!is-there-kryptonite)
-{
-    return "Superman beats " +enemyName+ ", of course";
-}
-
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
-}
-    }
-
-for (var i=0;i<enemies.length;i++)
-{
-    var is_there_kryptonite:
-    if (i%2=== 0) {
-        is_there_kryptonite = true;
+// typo function name change to camelCase and its paramaters
+// also changed withing the function itself
+// indentation changed throughout function, curly brackets changed to current lines
+function whoWins(isThereKryptonite, enemyName) {
+    if (!isThereKryptonite) {
+        return "Superman beats " + enemyName + ", of course";
     } else {
-        is_there_kryptonite = false;
+        return "Depends on how quick Superman can get rid of the Kryptonite. " + enemyName + " could possibly win this one.";
     }
-    console.log(Who_Wins(is_there_kryptonite,enemies[i]));
+}
+// added whitespace, fixed indentation, fixed var names and parameters to camelCase
+for (var i = 0; i < enemies.length; i++) {
+    var isThereKryptonite;
+    if (i  % 2 === 0) {
+        isThereKryptonite = true;
+    } else {
+        isThereKryptonite = false;
+    }
+    console.log(whoWins(isThereKryptonite, enemies[i]));
 }
 
-function HowAttractedIsLoisLaneToMe ()
+// fixed name to camelCase, typo and white space
+function howAttractedIsLoisLaneToMe ()
 {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Mat.random()*10)+1);
+    // subtract 1 before multiply by random number
+    return Math.floor((Math.random() * (10 - 1) + 1));
 }
+// typo and whitespace fixed
+console.log(howAttractedIsLoisLaneToMe());
 
-consol.log( HowAttractedIsLoisLaneToMe ()) ;
+// white space and assign value to superman, wrap var in 
+var clarkKent = true;
+var superman = false;
 
-var clarkKent =true;
-var superman false;
-
+// fixed indentation, whitespace added
 while (clarkKent){
     console.log("I'm just a nerdy columnist");
-    var phoneBoothQuickChange =Math.random();
-    if (phoneBoothQuickChange>= 0.5) {
-        clarkKent=false;
-        superman=true;
-        console.log("Now I'm Superman!");}}
+    var phoneBoothQuickChange = Math.random();
+    if (phoneBoothQuickChange >= 0.5) {
+        clarkKent = false;
+        superman = true;
+        console.log("Now I'm Superman!");
+    }
+}
