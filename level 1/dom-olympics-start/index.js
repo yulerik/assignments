ae9add33ff71fb9672aba697f240234ad5d409d9
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2021-09-30 13:31:01 -0700

    added dom manipulation to chat box. first modifed left side messages to censor every other word. right side messages change every other letter to "#". Added event listener to clear button, once pressed all chat clears.

--- level 1/dom-olympics-start/index.js
+++ level 1/dom-olympics-start/index.js
@@ -1,3 +1,4 @@
+// new header
 var header = document.getElementById('header');
 var newHeader = document.createElement('h1');
 newHeader.textContent = 'JavaScript Made This!!';
@@ -6,3 +7,58 @@ header.style.textAlign = 'center';
 var headerPara = document.createElement('p');
 headerPara.innerHTML = '<span id="name">Forrest</span> wrote this JavaScript';
 header.appendChild(headerPara);
+
+// chat update and clear command                                                                                                                                                                                                                                                                                                                                                                     
+var main = document.getElementById('main');
+var chat = document.getElementsByClassName('messages');
+var left = document.getElementsByClassName('message left');
+var right = document.getElementsByClassName('message right');
+var clearChat = document.getElementById('clear-button');
+
+// iterate over every chat on left side
+// change every other word to be censored
+for (var i = 0; i < left.length; i++) {
+    var current = left[i].innerHTML;
+    var words = current.split(' ');
+    for (var j = 0; j < words.length; j+= 2) {
+        var letters = words[j].split('');
+        for (var k = 0; k < letters.length; k++) {
+            letters[k] = '*';
+        }
+        words[j] = letters.join('');
+    }
+    left[i].innerHTML = words.join(' ');
+}
+
+// iterate over every chat on right side
+// change every other letter, in a word, to a pound sign
+for (var i = 0; i < right.length; i++) {
+    var current = right[i].innerHTML;
+    var words = current.split(' ');
+    for (var j = 0; j < words.length; j++) {
+        var letters = words[j].split('');
+        if (letters.length % 2 === 0) {
+            for (var k = 0; k < letters.length; k+= 2) {
+                letters[k] = '#';
+            }
+        } else {
+            for (var k = 1; k < letters.length; k+= 2) {
+                letters[k] = '#';
+            }
+        }
+        
+        words[j] = letters.join('');
+    }
+    right[i].innerHTML = words.join(' ');
+}
+
+// clear chat when clear button is clicked
+function chatCleared() {
+    alert('chat will clear');
+    var oldChat = chat[0];
+    while (oldChat.firstElementChild) {
+        oldChat.removeChild(oldChat.firstElementChild);
+    }
+}
+
+clearChat.addEventListener('click', chatCleared);
