79921477b63a531ca589c12212f1cc8619acd447
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2021-10-13 16:34:31 -0700

    basic styling. function works properly. after submit, alert pops with user information that they entered.

--- level 1/travel-form/main.js
+++ level 1/travel-form/main.js
@@ -1,8 +1,29 @@
 const customerInfo = document.travelInfo
 
 function getInfo(event) {
-    alert('working')
+    event.preventDefault();
     console.dir(customerInfo)
+    const firstName = customerInfo.firstName.value;
+    console.log(firstName)
+    const lastName = customerInfo.lastName.value;
+    const age = customerInfo.age.value;
+    const gender = customerInfo.gender.value;
+    const location = customerInfo.location;
+    const destinations = [];
+    for (let i = 0; i < location.length; i++) {
+        if (location[i].checked) {
+            destinations.push(` ${location[i].value}`);
+        }
+    }
+    const diet = customerInfo.diet;
+    const restrictions = [];
+    for (let i = 0; i < diet.length; i++) {
+        if (diet[i].checked) {
+            restrictions.push(` ${diet[i].value}`);
+        }
+    }
+    alert(`First name: ${firstName}\nLast name: ${lastName}\nAge: ${age}\nGender: ${gender}\nLocation(s): ${destinations}\nDietary restrictions: ${restrictions}`)
+    
 }
 
 customerInfo.addEventListener('submit', getInfo)
