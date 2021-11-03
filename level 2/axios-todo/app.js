2a06878fdc5c37d255ea27dc6a68dfdc01bea367
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2021-11-02 17:13:58 -0700

    Finished the functionality of the todo list. input form adds items to database and shows on screen without refresh. complete checkmark strikesthrough text and lowers opacity of image. talks with datapas with a put call and updates accordingly. Delete button delete item from list and database with a delete call. edit button clears old list item and shows a new form. Can change previous list item data, and save to the database. PUT call with new values to the database. response calls function get call to restart process of populating main body with correct items from most recent get call. basic styling, no colors just positioning. Functionality works, looks and responsiveness still need some work.

--- level 2/axios-todo/app.js
+++ level 2/axios-todo/app.js
@@ -1,5 +1,5 @@
 const todo = document.todo;
-// GET call to api, call showList to refresh page with updated list
+// GET call to api, call showList to refresh page with updated list items
 function mainList() {
     axios.get("https://api.vschool.io/yuleriksorensen/todo")
         .then(response => showList(response.data))
@@ -8,16 +8,20 @@ function mainList() {
 // puts data from GET call onto the screen
 function showList(data) {   
     // document.getElementById('main').innerHTML = '';
+    // clear old data from page
     clearMain()
+    // empty form inputs
     clearForm()
-
+    // for loop for each data id for the list
     for (let i = 0; i < data.length; i++) {
         // create div to wrap buttons and list items, current id
+        // parent div for edit div and list items div
         const eachListItem = document.createElement('div');
         eachListItem.id = data[i]['_id'];
         eachListItem.className = 'item'
 
         // list items from database
+        // div for title, price, desc, img
         const listItem = document.createElement('div');
         listItem.className = 'listItems';
         // title of list item
@@ -26,7 +30,7 @@ function showList(data) {
         listItem.appendChild(h1);
         // price of list item
         const price = document.createElement('h6');
-        price.textContent = `Price: ${data[i].price}`;
+        price.textContent = data[i].price;
         listItem.appendChild(price);
         // description of list item
         const desc = document.createElement('p');
@@ -36,39 +40,49 @@ function showList(data) {
         const image = document.createElement('img');
         image.src = data[i].imgUrl;
         listItem.appendChild(image);
-
+        // append div to parent div
         eachListItem.appendChild(listItem);
 
         // create edit div for checkbox, delete, and edit
+        // div to wrap buttons
         const edit = document.createElement('div');
         edit.className = 'editOptions';
-        
+
+        // form for checkbox for completeing tasks
         const completeTask = document.createElement('form');
         completeTask.name = 'complete-task';
+        // checkbox input
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
+        // assign checkbox checked boolean value with 
+        // current completed value from GET call
         checkbox.value = data[i]['_id'];
-        // assign checkbox checked boolean with response from server data
         checkbox.checked = data[i]["completed"];
-        if (checkbox.checked === true) {
-            strikethrough(listItem);
-        }
-        // call strikethrough function for checkbox, if true strikethrough
+        // strikethrough when current completed status is true
+        if (checkbox.checked) {strikethrough(listItem);}
         checkbox.name = 'completed';
         completeTask.appendChild(checkbox);
+        // label for checkbox
         const check = document.createElement('label');
         check.for = 'completed';
         check.textContent = 'Completed';
         completeTask.appendChild(check);
         edit.appendChild(completeTask);
+        // event listener to PUT call function for completing tasks
         checkbox.addEventListener('click', isCompleted);
 
+        // edit button
         const editButton = document.createElement('input');
         editButton.type = 'button';
+        // set name to id from database for ease of access later
+        // when needing to delete
+        editButton.name = data[i]['_id'];
         editButton.value = "Edit";
         edit.appendChild(editButton);
+        // event listener to call function PUT call to edit input item
         editButton.addEventListener('click', editListItem);
 
+        // delete button
         const deleteButton = document.createElement('button');
         deleteButton.type = 'button';
         // set name to id from database for ease of access later
@@ -76,9 +90,10 @@ function showList(data) {
         deleteButton.name = data[i]['_id'];
         deleteButton.textContent = 'DELETE';
         edit.appendChild(deleteButton);
-        eachListItem.appendChild(edit);
         deleteButton.addEventListener('click', deleteListItem);
-        // append list item wrapped div to main element
+        // add edit div to parent div
+        eachListItem.appendChild(edit);
+        // add parent div of edit and items to main document
         document.getElementById('main').appendChild(eachListItem);
 
     }
@@ -90,9 +105,9 @@ function clearMain(){
         mainBody.removeChild(mainBody.firstChild)
     }
 }
-
+// GET call to start,
 mainList();
-
+// event listener to post form to database
 todo.addEventListener("submit", postForm);
 // creates new list item with form input values
 function postForm(event) {
@@ -108,7 +123,6 @@ function postForm(event) {
     axios.post("https://api.vschool.io/yuleriksorensen/todo", newItem)
         .then(response => mainList(response.data))
         .catch(error => console.log(error))
-
 }
 // input form fields are set to blank strings
 function clearForm() {
@@ -120,9 +134,100 @@ function clearForm() {
 // calls when edit button is clicked
 function editListItem(event){
     event.preventDefault()
-    alert('editing')
+    // clear eventlistener for current div, to replace with save event listener
+    event.target.removeEventListener('click', editListItem); 
+
+    // _id for PUT call is the same as the name for the edit button
+    const itemId = event.target.name;
+    console.dir(itemId);
+    // use button name for currentDiv
+    const currentDiv = document.getElementById(itemId);
+    // access the list items title, price, desc, imgUrl
+    const listItem = currentDiv.firstChild;
+    const [title, price, description, imageUrl] = listItem.children;
+    
+    // hide current list item, but not edit button
+    currentDiv.children[0].style.display = 'none';
+    // change edit button to save and add event listener for PUT call function
+    // to update the form
+    currentDiv.children[1].children[1].value = 'Save';
+    currentDiv.children[1].children[1].addEventListener('click', editPutCall)
+
+    // create form to place ontop of edit div
+    const editForm = document.createElement('form');
+    editForm.name = 'save';
+    // create four inputs with values as old list item values
+    const titleLabel = document.createElement('label');
+    titleLabel.for = 'newTitle';
+    titleLabel.type = 'text';
+    titleLabel.textContent = 'Title';
+    const titleInput = document.createElement('input');
+    titleInput.name = 'newTitle';
+    titleInput.type = 'text';
+    titleInput.value = title.textContent;
+    const priceLabel = document.createElement('label');
+    priceLabel.for = 'newPrice';
+    priceLabel.type = 'text';
+    priceLabel.textContent = 'Price';
+    const priceInput = document.createElement('input');
+    priceInput.name = 'newPrice';
+    priceInput.value = price.textContent;
+    priceInput.type = 'text';
+    const descriptionLabel = document.createElement('label');
+    descriptionLabel.for = 'newDescription';
+    descriptionLabel.type = 'text';
+    descriptionLabel.textContent = 'Description';
+    const descriptionInput = document.createElement('input');
+    descriptionInput.name = 'newDescription';
+    descriptionInput.value = description.textContent;
+    descriptionInput.type = 'text';
+    const imageLabel = document.createElement('label');
+    imageLabel.for = 'newImageUrl';
+    imageLabel.type = 'text';
+    imageLabel.textContent = 'Image URL';
+    const imageInput = document.createElement('input');
+    imageInput.name = 'newImageUrl';
+    imageInput.value = imageUrl.src;
+    imageInput.type = 'text';
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Save';
+    saveButton.addEventListener('click', editPutCall);
+    editForm.appendChild(titleLabel);
+    editForm.appendChild(titleInput);
+    editForm.appendChild(priceLabel);
+    editForm.appendChild(priceInput);
+    editForm.appendChild(descriptionLabel);
+    editForm.appendChild(descriptionInput);
+    editForm.appendChild(imageLabel);
+    editForm.appendChild(imageInput);
+    editForm.appendChild(saveButton);
+
+    currentDiv.appendChild(editForm);
 }
-// calls when delete button is clicked
+// PUT call to edit current ID items
+function editPutCall(event){
+    event.preventDefault();
+    // parent div id is _id for database
+    const itemId = event.target.parentElement.parentElement.id;
+    // get the values of the save form inputs
+    const newTitle = document.save.newTitle.value;
+    const newPrice = document.save.newPrice.value;
+    const newDescription = document.save.newDescription.value;
+    const newImageUrl = document.save.newImageUrl.value;
+    // JSON to update database on PUT call
+    const updateItem = {
+        "title": newTitle,
+        "description": newDescription,
+        "price": newPrice,
+        "imgUrl": newImageUrl
+    }
+    // PUT call with proper response recalls mainList to repopulate data to main body
+    axios.put(`https://api.vschool.io/yuleriksorensen/todo/${itemId}`, updateItem)
+        .then(response => mainList())
+        .catch(error => console.log(error))
+}
+
+// PUT call to delete current ID from database
 function deleteListItem(event){
     event.preventDefault();
     alert('this will delete item');
