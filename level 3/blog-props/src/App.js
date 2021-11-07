6aebb6757c8acc3ff925ac631bfa5ebb7a0972df
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2021-11-06 22:22:28 -0700

    initial commit

---
+++ level 3/blog-props/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Styles from './styles.css'
+import blogPosts from './postHistory'
+
+function App(props) {
+    return (
+        <h1>hello world</h1>
+    )
+}
+
+export default App
