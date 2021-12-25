314701cd3fa847439602d160e070403b15613b3d
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2021-12-24 20:25:04 -0800

    basic app with routes

---
+++ level 4/roto-router/src/About.js
@@ -0,0 +1,9 @@
+// import react from 'react'
+
+function About() {
+    return (
+        <h1>About Page</h1>
+    )
+}
+
+export default About
---
+++ level 4/roto-router/src/Services.js
@@ -0,0 +1,9 @@
+// import react from 'react'
+
+function Services() {
+    return (
+        <h1>Services Page</h1>
+    )
+}
+
+export default Services
